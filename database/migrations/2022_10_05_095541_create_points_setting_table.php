058d70e23601c6c14ec6e791021ec94f50f755d6
fadi ayyas <fadi ayyas>
2022-10-05 13:05:05 +0300

    modifiy 2022/10/5 v2

---
+++ database/migrations/2022_10_05_095541_create_points_setting_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePointsSettingTable extends Migration
+{
+
+    public function up()
+    {
+        Schema::create('products_points', function (Blueprint $table) {
+            $table->id();
+            $table->string('type');
+            $table->bigInteger('type_id');
+            $table->string('name');
+            $table->bigInteger('quantity');
+            $table->bigInteger('points');
+            $table->timestamps();
+        });
+
+        Schema::create('orders_points', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('price');
+            $table->bigInteger('points');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('points_setting');
+    }
+}
